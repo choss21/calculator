4266b3e34a8dbf1ed3697ad961e44a533ce88da4
BryanRodriguez <brayanjosue973@gmail.com>
2020-07-08 23:23:15 -0500

    cambio

---
+++ MathLib/OperacionesAritmeticas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLib
+{
+    public abstract class OperacionesAritmeticas
+    {
+        public double Numero1 { get; set; }
+        public double Numero2 { get; set; }
+        public double Resultado { get; set; }
+    }
+}
