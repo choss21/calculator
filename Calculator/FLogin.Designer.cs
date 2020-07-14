6de92c2108b6d90b103a3099f3cd47ade7f30ae6
choss <choss_21@hotmail.com>
2020-07-14 00:12:53 -0500

    ejemplo base de datos

---
+++ Calculator/FLogin.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Calculator
+{
+    partial class FLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnVerContraseña = new System.Windows.Forms.Button();
+            this.gridAlumnos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.gridAlumnos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuario:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Contraseña";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(129, 47);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(316, 29);
+            this.txtUsuario.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(129, 101);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(316, 29);
+            this.txtPassword.TabIndex = 3;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(118, 176);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(145, 33);
+            this.btnAceptar.TabIndex = 4;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(289, 176);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 33);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Cancelar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // btnVerContraseña
+            // 
+            this.btnVerContraseña.Location = new System.Drawing.Point(449, 102);
+            this.btnVerContraseña.Name = "btnVerContraseña";
+            this.btnVerContraseña.Size = new System.Drawing.Size(64, 29);
+            this.btnVerContraseña.TabIndex = 6;
+            this.btnVerContraseña.Text = "ver";
+            this.btnVerContraseña.UseVisualStyleBackColor = true;
+            this.btnVerContraseña.Click += new System.EventHandler(this.btnVerContraseña_Click);
+            // 
+            // gridAlumnos
+            // 
+            this.gridAlumnos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gridAlumnos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridAlumnos.Location = new System.Drawing.Point(20, 248);
+            this.gridAlumnos.Name = "gridAlumnos";
+            this.gridAlumnos.Size = new System.Drawing.Size(525, 182);
+            this.gridAlumnos.TabIndex = 7;
+            // 
+            // FLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 442);
+            this.Controls.Add(this.gridAlumnos);
+            this.Controls.Add(this.btnVerContraseña);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "FLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Iniciar sesion al Gestionador de Alumnos";
+            ((System.ComponentModel.ISupportInitialize)(this.gridAlumnos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnVerContraseña;
+        private System.Windows.Forms.DataGridView gridAlumnos;
+    }
+}
---
+++ Calculator/FLogin.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class FLogin : Form
+    {
+        public FLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerContraseña_Click(object sender, EventArgs e)
+        {
+            txtPassword.UseSystemPasswordChar = !txtPassword.UseSystemPasswordChar;
+
+            //if (txtPassword.UseSystemPasswordChar == true)
+            //{
+            //    txtPassword.UseSystemPasswordChar = false;
+            //}
+            //else
+            //{
+            //    txtPassword.UseSystemPasswordChar = true;
+            //}
+
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            //Ejemplo de conexion a base de datos
+            var cadenaDeConexion = "server=(local); database=Alumnos ; integrated security=true";
+            var conexion = new SqlConnection(cadenaDeConexion);
+
+            SqlCommand command = new SqlCommand("SELECT * FROM Alumnos;", conexion);
+            conexion.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+            var alumnos = new List<Alumno>();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}", reader.GetInt32(0),
+                        reader.GetString(1),
+                        reader.GetString(2),
+                        reader.GetString(3),
+                        reader.GetDateTime(4));
+                    alumnos.Add(new Alumno()
+                    {
+                        AlumnoId = reader.GetInt32(0),
+                        Nombres = reader.GetString(1),
+                        ApellidoMaterno = reader.GetString(2),
+                        ApellidoPaterno = reader.GetString(3),
+                        FechaNacimiento = reader.GetDateTime(4)
+                    });
+                }
+            }
+            else
+            {
+                Console.WriteLine("No rows found.");
+            }
+            reader.Close();
+
+
+
+            conexion.Close();
+            gridAlumnos.DataSource = alumnos;
+
+        }
+    }
+}
