1d360a04f9cad1cce8d8f03193707998af43765a
choss <choss_21@hotmail.com>
2020-08-03 23:17:31 -0500

    askldj

---
+++ Calculator.Model/Context/Mapping/AlumnoMapping.cs
@@ -0,0 +1,34 @@
+﻿using Calculator.BO;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Model.Context.Mapping
+{
+    public class AlumnoMapping : EntityTypeConfiguration<Alumno>
+    {
+        public AlumnoMapping()
+        {
+            ToTable("Alumnos");
+            HasKey(x => x.Matricula);
+            Property(x => x.Matricula)
+                .IsRequired()
+                .HasDatabaseGeneratedOption(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.None);
+
+            Property(x => x.Nombres)
+                .IsRequired()
+                .HasMaxLength(100);
+            Property(x => x.ApellidoPaterno)
+               .IsRequired()
+               .HasMaxLength(50);
+            Property(x => x.ApellidoMaterno)
+               .IsRequired()
+               .HasMaxLength(50);
+
+
+        }
+    }
+}
--- Calculator.Model/Context/ModelContext.cs
+++ Calculator.Model/Context/ModelContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Calculator.BO;
+using Calculator.Model.Context.Mapping;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -9,10 +11,18 @@ namespace Calculator.Model.Context
 {
     public class ModelContext:DbContext
     {
+        public DbSet<Alumno> Alumnos { get; set; }
+        public DbSet<Carrera> Carreras { get; set; }
         public ModelContext():base("CalculatorBD")
         {
-
+            Configuration.LazyLoadingEnabled = true;
         }
-        
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //aqui es donde voy a configurar mis mappings
+            modelBuilder.Configurations.Add(new AlumnoMapping());
+            modelBuilder.Configurations.Add(new CarreraMapping());
+        }
+
     }
 }
