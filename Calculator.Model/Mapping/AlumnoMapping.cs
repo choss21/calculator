f8e1597d00a8d992d425f0d69502d4069203aee0
choss <choss_21@hotmail.com>
2020-08-03 23:14:20 -0500

    PArcial de entityFramework

---
+++ Calculator.Model/Mapping/AlumnoMapping.cs
@@ -0,0 +1,45 @@
+﻿using Calculator.BO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Model.Mapping
+{
+    public class AlumnoMapping : EntityTypeConfiguration<Alumno>
+    {
+        public AlumnoMapping()
+        {
+            ToTable("Alumnos");
+            HasKey(x => x.Matricula);
+            Property(x => x.Matricula)
+                .IsRequired()
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.None);
+
+            Property(x => x.Nombres)
+                .IsRequired()
+                .HasMaxLength(100);
+            Property(x => x.ApellidoPaterno)
+                .IsRequired()
+                .HasMaxLength(50);
+            Property(x => x.ApellidoMaterno)
+                .IsOptional()
+                .HasMaxLength(50);
+            Property(x => x.FechaNacimiento)
+                .IsRequired();
+            Property(x => x.Correo)
+                .IsOptional()
+                .HasMaxLength(50);
+
+            HasRequired(x => x.Carrera)
+                .WithMany()
+                .HasForeignKey(x => x.CarreraId);
+
+
+
+        }
+    }
+}
---
+++ Calculator/FAlumnos.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace Calculator
+{
+    partial class FAlumnos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridAlumnos = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMatricula = new System.Windows.Forms.TextBox();
+            this.txtNombres = new System.Windows.Forms.TextBox();
+            this.txtApellidoPaterno = new System.Windows.Forms.TextBox();
+            this.txtApellidoMaterno = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dtpFechaNacimiento = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            this.cmbCarrera = new System.Windows.Forms.ComboBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtCarrera = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtClaveCarrera = new System.Windows.Forms.TextBox();
+            this.btnGuardarCarrera = new System.Windows.Forms.Button();
+            this.gridCarreras = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.gridAlumnos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridCarreras)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridAlumnos
+            // 
+            this.gridAlumnos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gridAlumnos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridAlumnos.Location = new System.Drawing.Point(13, 262);
+            this.gridAlumnos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gridAlumnos.Name = "gridAlumnos";
+            this.gridAlumnos.Size = new System.Drawing.Size(685, 440);
+            this.gridAlumnos.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 12);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Matricula:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 42);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombres:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 74);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Apellido Paterno:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 104);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 18);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Apellido Materno:";
+            // 
+            // txtMatricula
+            // 
+            this.txtMatricula.Location = new System.Drawing.Point(186, 8);
+            this.txtMatricula.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtMatricula.Name = "txtMatricula";
+            this.txtMatricula.Size = new System.Drawing.Size(337, 24);
+            this.txtMatricula.TabIndex = 5;
+            // 
+            // txtNombres
+            // 
+            this.txtNombres.Location = new System.Drawing.Point(186, 38);
+            this.txtNombres.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtNombres.Name = "txtNombres";
+            this.txtNombres.Size = new System.Drawing.Size(337, 24);
+            this.txtNombres.TabIndex = 6;
+            // 
+            // txtApellidoPaterno
+            // 
+            this.txtApellidoPaterno.Location = new System.Drawing.Point(186, 68);
+            this.txtApellidoPaterno.Margin = new System.Windows.Forms.Padding(4);
+            this.txtApellidoPaterno.Name = "txtApellidoPaterno";
+            this.txtApellidoPaterno.Size = new System.Drawing.Size(337, 24);
+            this.txtApellidoPaterno.TabIndex = 7;
+            // 
+            // txtApellidoMaterno
+            // 
+            this.txtApellidoMaterno.Location = new System.Drawing.Point(186, 100);
+            this.txtApellidoMaterno.Margin = new System.Windows.Forms.Padding(4);
+            this.txtApellidoMaterno.Name = "txtApellidoMaterno";
+            this.txtApellidoMaterno.Size = new System.Drawing.Size(337, 24);
+            this.txtApellidoMaterno.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 135);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(132, 18);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Fecha Nacimiento:";
+            // 
+            // dtpFechaNacimiento
+            // 
+            this.dtpFechaNacimiento.CustomFormat = "dd/MMMM/yyyy";
+            this.dtpFechaNacimiento.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpFechaNacimiento.Location = new System.Drawing.Point(186, 129);
+            this.dtpFechaNacimiento.Name = "dtpFechaNacimiento";
+            this.dtpFechaNacimiento.Size = new System.Drawing.Size(337, 24);
+            this.dtpFechaNacimiento.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 167);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 18);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Correo:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(15, 194);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 18);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Carrera:";
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.Location = new System.Drawing.Point(186, 164);
+            this.txtCorreo.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(337, 24);
+            this.txtCorreo.TabIndex = 13;
+            // 
+            // cmbCarrera
+            // 
+            this.cmbCarrera.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCarrera.FormattingEnabled = true;
+            this.cmbCarrera.Location = new System.Drawing.Point(186, 191);
+            this.cmbCarrera.Name = "cmbCarrera";
+            this.cmbCarrera.Size = new System.Drawing.Size(337, 26);
+            this.cmbCarrera.TabIndex = 14;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(418, 226);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(104, 29);
+            this.btnGuardar.TabIndex = 15;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(656, 44);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(62, 18);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Carrera:";
+            // 
+            // txtCarrera
+            // 
+            this.txtCarrera.Location = new System.Drawing.Point(717, 41);
+            this.txtCarrera.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCarrera.Name = "txtCarrera";
+            this.txtCarrera.Size = new System.Drawing.Size(261, 24);
+            this.txtCarrera.TabIndex = 17;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(656, 74);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(49, 18);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Clave:";
+            // 
+            // txtClaveCarrera
+            // 
+            this.txtClaveCarrera.Location = new System.Drawing.Point(717, 68);
+            this.txtClaveCarrera.Margin = new System.Windows.Forms.Padding(4);
+            this.txtClaveCarrera.Name = "txtClaveCarrera";
+            this.txtClaveCarrera.Size = new System.Drawing.Size(261, 24);
+            this.txtClaveCarrera.TabIndex = 19;
+            // 
+            // btnGuardarCarrera
+            // 
+            this.btnGuardarCarrera.Location = new System.Drawing.Point(884, 104);
+            this.btnGuardarCarrera.Name = "btnGuardarCarrera";
+            this.btnGuardarCarrera.Size = new System.Drawing.Size(104, 29);
+            this.btnGuardarCarrera.TabIndex = 20;
+            this.btnGuardarCarrera.Text = "Guardar";
+            this.btnGuardarCarrera.UseVisualStyleBackColor = true;
+            this.btnGuardarCarrera.Click += new System.EventHandler(this.btnGuardarCarrera_Click);
+            // 
+            // gridCarreras
+            // 
+            this.gridCarreras.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gridCarreras.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridCarreras.Location = new System.Drawing.Point(706, 262);
+            this.gridCarreras.Margin = new System.Windows.Forms.Padding(4);
+            this.gridCarreras.Name = "gridCarreras";
+            this.gridCarreras.Size = new System.Drawing.Size(282, 440);
+            this.gridCarreras.TabIndex = 21;
+            // 
+            // FAlumnos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(991, 729);
+            this.Controls.Add(this.gridCarreras);
+            this.Controls.Add(this.btnGuardarCarrera);
+            this.Controls.Add(this.txtClaveCarrera);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtCarrera);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.cmbCarrera);
+            this.Controls.Add(this.txtCorreo);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dtpFechaNacimiento);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtApellidoMaterno);
+            this.Controls.Add(this.txtApellidoPaterno);
+            this.Controls.Add(this.txtNombres);
+            this.Controls.Add(this.txtMatricula);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gridAlumnos);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FAlumnos";
+            this.Text = "Listado de Alumnos";
+            ((System.ComponentModel.ISupportInitialize)(this.gridAlumnos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridCarreras)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gridAlumnos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMatricula;
+        private System.Windows.Forms.TextBox txtNombres;
+        private System.Windows.Forms.TextBox txtApellidoPaterno;
+        private System.Windows.Forms.TextBox txtApellidoMaterno;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dtpFechaNacimiento;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtCorreo;
+        private System.Windows.Forms.ComboBox cmbCarrera;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtCarrera;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtClaveCarrera;
+        private System.Windows.Forms.Button btnGuardarCarrera;
+        private System.Windows.Forms.DataGridView gridCarreras;
+    }
+}
