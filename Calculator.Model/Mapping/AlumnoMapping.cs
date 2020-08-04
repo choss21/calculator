bbeaf1459a72749df5fe9fa8dbda029273075cd7
VeroMV <veronica.mendoza.vidal@gmail.com>
2020-08-03 22:33:36 -0500

    Model

---
+++ Calculator.Model/Mapping/AlumnoMapping.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Model.Mapping
+{
+    public class AlumnoMapping : EntityTypeConfiguration<Alumno>
+    {
+
+    }
+}
