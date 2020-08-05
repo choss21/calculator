f759e2d2dce58c75edc0384cd844d1616012faa6
BryanRodriguez <brayanjosue973@gmail.com>
2020-08-04 22:56:39 -0500

    cambios

--- Calculator/FLogin.cs
+++ Calculator/FLogin.cs
@@ -36,43 +36,43 @@ namespace Calculator
         private void btnAceptar_Click(object sender, EventArgs e)
         {
             //Ejemplo de conexion a base de datos
-            var cadenaDeConexion = "server=(local); database=Alumnos; integrated security=true";
-            var conexion = new SqlConnection(cadenaDeConexion);
+            //var cadenaDeConexion = "server=(local); database=Alumnos; integrated security=true";
+            //var conexion = new SqlConnection(cadenaDeConexion);
 
-            SqlCommand command = new SqlCommand("SELECT * FROM Alumnos;", conexion);
-            conexion.Open();
+            //SqlCommand command = new SqlCommand("SELECT * FROM Alumnos;", conexion);
+            //conexion.Open();
 
-            SqlDataReader reader = command.ExecuteReader();
-            var alumnos = new List<Alumno>();
-            if (reader.HasRows)
-            {
-                while (reader.Read())
-                {
-                    Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}", reader.GetInt32(0),
-                        reader.GetString(1),
-                        reader.GetString(2),
-                        reader.GetString(3),
-                        reader.GetDateTime(4));
-                    alumnos.Add(new Alumno()
-                    {
-                        AlumnoId = reader.GetInt32(0),
-                        Nombres = reader.GetString(1),
-                        ApellidoMaterno = reader.GetString(2),
-                        ApellidoPaterno = reader.GetString(3),
-                        FechaNacimiento = reader.GetDateTime(4)
-                    });
-                }
-            }
-            else
-            {
-                Console.WriteLine("No rows found.");
-            }
-            reader.Close();
+            //SqlDataReader reader = command.ExecuteReader();
+            //var alumnos = new List<Alumno>();
+            //if (reader.HasRows)
+            //{
+            //    while (reader.Read())
+            //    {
+            //        Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}", reader.GetInt32(0),
+            //            reader.GetString(1),
+            //            reader.GetString(2),
+            //            reader.GetString(3),
+            //            reader.GetDateTime(4));
+            //        alumnos.Add(new Alumno()
+            //        {
+            //            AlumnoId = reader.GetInt32(0),
+            //            Nombres = reader.GetString(1),
+            //            ApellidoMaterno = reader.GetString(2),
+            //            ApellidoPaterno = reader.GetString(3),
+            //            FechaNacimiento = reader.GetDateTime(4)
+            //        });
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No rows found.");
+            //}
+            //reader.Close();
 
 
 
-            conexion.Close();
-            gridAlumnos.DataSource = alumnos;
+            //conexion.Close();
+            //gridAlumnos.DataSource = alumnos;
 
         }
     }
